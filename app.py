9423b6c2569db1ebccf0ae6d263b7f27c4de9f98
Melissa Mejía Villamil <mejiavillamil232@gmail.com>
2022-01-12 20:59:19 -0500

    Se realiza actualziación en condicionales para procesar información proveniente del API ya que el JSON que este envía es diferente al anteriormente configurado

--- app.py
+++ app.py
@@ -53,63 +53,78 @@ def json_example():
     id_message = js_string[-1]['id']
     last_json = js_string[-1]['message']
     replace_simple_quotation = str(last_json).replace("'", '"')
-    last_json = json.loads(replace_simple_quotation)  # Convierto a JSON mi str
+    replace_none_values = str(replace_simple_quotation).replace(" None", '""')
+    replace_true_value = str(replace_none_values).replace(" True", '"True"')
 
-    print('+++++++')
+    # Convierto a JSON mi str
+    last_json = json.loads(replace_true_value)
     print(last_json)
-    print(type(last_json))
 
     print(last_json['accion'])
-    print(last_json.get('accion'))
-    print(id_message)
-
-    if last_json.get('accion') == 'mover ':
-        escribirterminal("python pose.py")
-        print("escribí en la terminal: python pose.py ")
-
-    if last_json.get('accion') == 'Tomar ':
-        escribirterminal("python tomarfoto.py")
-        time.sleep(1)
-        escribirterminal("python mov_arms.py")
-        print("escribí en la terminal: python tomarfoto.py && python mov_arms.py ")
-
-    if last_json.get('accion') == 'Posicionar ':
-        escribirterminal("python TCI.py")
-        time.sleep(3)
-        print('escribí en la terminal: python TCI.py ')
-        # datos = open("pos_actual.txt", "r")
-        # valores = eval(datos.read())
-        # return(valores['estado'])
-
-    if last_json.get('accion') == 'iniciar':  # Home
-        print('inicié programa guardado')
-        # abrirterminal()
-        # escribirterminal("cd ros_ws")
-        # escribirterminal(". baxter.sh")
-        # escribirterminal("rosrun baxter_tools enable_robot.py -e")
+    # print(last_json.get('accion'))
+    # print(id_message)
+    if (last_json['confirmacion'] == 'True'):
+        if(last_json['accion'] == 'tomar'):
+            # escribirterminal("python tomarfoto.py")
+            print("escribí 'python tomarfoto.py' en la terminal ")
+            # time.sleep(1)
+            # escribirterminal("python mov_arms.py")
+            print("escribí 'python mov_arms.py' en la terminal ")
+        if(last_json['accion'] == "mover"):
+            #escribirterminal("python pose.py")
+            print("escribí 'python pose.py' en la terminal ")
+        if(last_json['accion'] == "guardar"):
+            print("guardé los valores en txts")
+            # os.rename("movimientosleft.txt", "Programas/" +
+            #             str(datos['nombre_programa'])+"l.txt")
+            # os.rename("movimientosright.txt", "Programas/" +
+            #             str(datos['nombre_programa'])+"r.txt")
+        if(last_json['accion'] == "ejecutar"):  # pendiente
+            # escribirterminal("python programas_guardadas.py")
+            print("escribí 'python programas_guardadas.py' en la terminal ")
+        if(last_json['accion'] == "posicionar"):
+            # escribirterminal("python TCI.py")
+            print("escribí 'python TCI.py' en la terminal ")
+            # time.sleep(3)
+            # datos = open("pos_actual.txt", "r")
+            # valores = eval(datos.read())
+            # return(valores['estado'])
         # time.sleep(10)
         # escribirterminal("python obtener_pos.py")
+        # datos = open("pos_actual.txt", "r")
+        # valores = eval(datos.read())
+        # return(valores)
 
-    if last_json.get('accion') == 'set':  # Set
-        print('seteé posición')
-        # escribirterminal("rosrun baxter_tools tuck_arms.py -u")
-        # borrar_registros()
-        # time.sleep(5)
+        if (last_json["accion"] == "iniciar"):
+            # abrirterminal()
+            print('Se ejecuta todo lo respectivo a INICIAR')
+            # escribirterminal("cd ros_ws")
+            # escribirterminal(". baxter.sh")
+            # escribirterminal("rosrun baxter_tools enable_robot.py -e")
+            # time.sleep(10)
+            # escribirterminal("python obtener_pos.py")
+        if (last_json["accion"] == "set"):
+            print('Se ejecuta todo lo respectivo a SET')
+            # escribirterminal("rosrun baxter_tools tuck_arms.py -u")
+            # borrar_registros()
+            # time.sleep(5)
+            # escribirterminal("python obtener_pos.py")
+        if(last_json["accion"] == "apagar"):
+            print('Se ejecuta todo lo respectivo a APAGAR')
+            # escribirterminal("rosrun baxter_tools tuck_arms.py -t")
+            # escribirterminal("exit")
+            # escribirterminal("exit")
+            # borrar_registros()
+        # print(payload["accion"])
+        # datos = open("pos_actual.txt", "r")
+        # #valores=eval(datos.read())
+        # return (datos.read())
+
+    else:
         # escribirterminal("python obtener_pos.py")
-
-    if last_json.get('accion') == 'save':  # Guardar
-        print('guardé la información')
-        # os.rename("movimientosleft.txt", "Programas/" +
-        #           str(datos['nombre_programa'])+"l.txt")
-        # os.rename("movimientosright.txt", "Programas/" +
-        #           str(datos['nombre_programa'])+"r.txt")
-
-    if last_json.get('accion') == 'apagar':  # Apagar
-        print('me apagué')
-        # escribirterminal("rosrun baxter_tools tuck_arms.py -t")
-        # escribirterminal("exit")
-        # escribirterminal("exit")
-        # borrar_registros()
+        print("escribí 'python obtener_pos.py' en la terminal ")
+        # datos = open("pos_actual.txt", "r")
+        # return (datos.read()) #Retornaba json con los valores de cada articulación del brazo
 
     return str(js_string)
 
